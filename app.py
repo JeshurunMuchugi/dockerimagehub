97df5658e9014cc8bb67eeda1a53351158fb1dae
JeshurunMuchugi <jeshurunmuchugi@gmail.com>
2026-02-10 11:46:30 +0300

    cicd yml file ready

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
+app=Flask(__name__)
+
+@app.route("/")
 def home():
-    return "Welcome to the home page!"
+    return "Hello World!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000)
